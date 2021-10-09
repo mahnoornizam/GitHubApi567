ec941e0c49103c1c151bfc3707e476a97d1d44de
mahnoornizam <90106364+mahnoornizam@users.noreply.github.com>
2021-10-09 09:45:20 -0400

    Update github_repo_test.py

--- github_repo_test.py
+++ github_repo_test.py
@@ -1,21 +1,22 @@
 import unittest
-from unittest.mock import MagicMock,patch
-from githubrepo import *
-class Testget_repositories(unittest.TestCase):
-    @patch("githubrepo.get_repositories")
-    def testget_repositories(self,Mocktest):
-        list1 = ['Repo: csp Commits number: 2', 
-                 'Repo: hellogitworld Commits number: 30',
-                 'Repo: helloworld Commits number: 6',
-                 'Repo: Mocks Commits number: 10', 
-                 'Repo: Project1 Commits number: 2', 
-                 'Repo: richkempinski.github.io Commits number: 9',
-                 'Repo: threads-of-life Commits number: 1',
-                 'Repo: try_nbdev Commits number: 2',
-                 'Repo: try_nbdev2 Commits number: 5']
-        self.assertEqual('Repo: csp Commits number: 2', list1[0])
+
+from githubrepo import getGitHubRepos, getGitHubRepoCommits
+
+class TestGitHub(unittest.TestCase):
+
+    #Test  1
+    def testGitHubRepos1(self): 
+        self.assertEqual(getGitHubRepos('richkempinski'),['csp', 'hellogitworld', 'helloworld', 'Mocks', 'Project1', 'richkempinski.github.io', 'threads-of-life', 'try_nbdev', 'try_nbdev2'])
+
+    #Test  2
+    def testGitHubRepos2(self): 
+        self.assertEqual(getGitHubRepos('richkpinski'),'User ID was not found')
+    
+    #Test  3
+    def testGitHubRepoCommits1(self): 
+        self.assertEqual(getGitHubRepoCommits('richkempinski', 'hellogitworld'), 30)
 
 if __name__ == '__main__':
-    print('tests begin')
-    unittest.main(exit=False, verbosity=2)
+    print('Running unit tests')
+    unittest.main()
 
